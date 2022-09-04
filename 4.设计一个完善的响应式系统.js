c10d1e50301384d8935b122abe0a202bda82bc46
ranqirong <274544338@qq.com>
2022-09-04 15:25:33 +0800

    4.3 拦截get-关联副作用函数

--- 4.设计一个完善的响应式系统.js
+++ 4.设计一个完善的响应式系统.js
@@ -1,21 +1,31 @@
 /*
  * @Description:
- * 1. 代理拦截data的set属性 -> 给data赋值时,自动执行副作用函数
- * 2. 手动执行副作用函数 -> 根据data的值修改网页
- * 3. 修改data的值 -> 执行set拦截器
- * @Date: 2022-09-04 14:26:30
+ * 1. 代理拦截data的get属性 -> 将key和副作用函数关联起来
+ * 2. 代理拦截data的set属性 -> 给data赋值时,自动执行[对应key]的副作用函数
+ * 3. 手动执行副作用函数 -> 执行get拦截器,根据data的值修改网页
+ * 4. 修改data的值 -> 执行set拦截器
+ * @Date: 2022-09-04 14:38:11
  * @Author: ranwawa <ranwawa@zmn.cn>
  */
 
 const data = { name: "冉娃娃", age: 18 };
 
+const bucket = {};
+
 const proxyData = new Proxy(data, {
+  get(target, key) {
+    bucket[key] = effect;
+
+    return target[key];
+  },
   set(target, key, newValue) {
     target[key] = newValue;
 
-    effect({ key });
+    if (!bucket[key]) {
+      return true;
+    }
 
-    return true;
+    bucket[key]({ key });
   },
 });
 
@@ -31,7 +41,7 @@ setTimeout(() => {
   proxyData.name = "帅气的冉娃娃";
 }, 1000);
 
-// TODO age在网页上没有用到,也触发了副作用函数
+// TODO 如果网页上要用到age怎么办?
 setTimeout(() => {
   proxyData.age = 28;
 }, 1000);
