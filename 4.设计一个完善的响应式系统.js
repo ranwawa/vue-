6ef87f306fa548e3af8f0ae3ffb98fa6ddd64ced
ranqirong <274544338@qq.com>
2022-09-04 15:24:07 +0800

    4.2 拦截set-自动执行副作用函数

--- 4.设计一个完善的响应式系统.js
+++ 4.设计一个完善的响应式系统.js
@@ -1,21 +1,37 @@
 /*
  * @Description:
- * 1. 手动执行副作用函数 -> 根据data的值修改网页
- * @Date: 2022-09-04 14:24:41
+ * 1. 代理拦截data的set属性 -> 给data赋值时,自动执行副作用函数
+ * 2. 手动执行副作用函数 -> 根据data的值修改网页
+ * 3. 修改data的值 -> 执行set拦截器
+ * @Date: 2022-09-04 14:26:30
  * @Author: ranwawa <ranwawa@zmn.cn>
  */
 
 const data = { name: "冉娃娃", age: 18 };
 
-function effect() {
-  console.log("执行副作用函数,修改网页上的值");
+const proxyData = new Proxy(data, {
+  set(target, key, newValue) {
+    target[key] = newValue;
 
-  document.querySelector("#text").innerHTML = data.name;
+    effect({ key });
+
+    return true;
+  },
+});
+
+function effect({ key } = {}) {
+  console.log("执行副作用函数,修改网页上的值", key);
+
+  document.querySelector("#text").innerHTML = proxyData.name;
 }
 
 effect();
 
-// TODO: 值变了,页面上的内容没有变?
 setTimeout(() => {
-  data.name = "帅气的冉娃娃";
+  proxyData.name = "帅气的冉娃娃";
+}, 1000);
+
+// TODO age在网页上没有用到,也触发了副作用函数
+setTimeout(() => {
+  proxyData.age = 28;
 }, 1000);
