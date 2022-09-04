6262521ac2c297667755a7395fb296750077f9b5
ranqirong <274544338@qq.com>
2022-09-04 15:43:30 +0800

    4.4 创建工厂函数-自定义副作用函数

--- 4.设计一个完善的响应式系统.js
+++ 4.设计一个完善的响应式系统.js
@@ -2,19 +2,21 @@
  * @Description:
  * 1. 代理拦截data的get属性 -> 将key和副作用函数关联起来
  * 2. 代理拦截data的set属性 -> 给data赋值时,自动执行[对应key]的副作用函数
- * 3. 手动执行副作用函数 -> 执行get拦截器,根据data的值修改网页
- * 4. 修改data的值 -> 执行set拦截器
+ * 3. 创建工厂函数 -> 利用全局变量自定义每个key的副作用函数
+ * 4. 手动执行副作用函数 -> 执行get拦截器,根据data的值修改网页
+ * 5. 修改data的值 -> 执行set拦截器
  * @Date: 2022-09-04 14:38:11
  * @Author: ranwawa <ranwawa@zmn.cn>
  */
 
-const data = { name: "冉娃娃", age: 18 };
+const data = { name: "冉娃娃", age: 18, showAge: true };
 
+let activeEffect = undefined;
 const bucket = {};
 
 const proxyData = new Proxy(data, {
   get(target, key) {
-    bucket[key] = effect;
+    bucket[key] = activeEffect;
 
     return target[key];
   },
@@ -29,19 +31,34 @@ const proxyData = new Proxy(data, {
   },
 });
 
-function effect({ key } = {}) {
-  console.log("执行副作用函数,修改网页上的值", key);
+function effectFactory(effectFn) {
+  const wrappedEffectFn = ({ key } = {}) => {
+    console.log("执行副作用函数,修改网页上的值", key);
 
-  document.querySelector("#text").innerHTML = proxyData.name;
+    activeEffect = wrappedEffectFn;
+
+    effectFn();
+  };
+
+  wrappedEffectFn();
 }
 
-effect();
+effectFactory(() => {
+  document.querySelector("#text").innerHTML = proxyData.name;
+});
+
+effectFactory(() => {
+  document.querySelector("#age").innerHTML = proxyData.showAge
+    ? proxyData.age
+    : "不显示年龄";
+});
 
 setTimeout(() => {
   proxyData.name = "帅气的冉娃娃";
+  proxyData.showAge = false;
 }, 1000);
 
-// TODO 如果网页上要用到age怎么办?
 setTimeout(() => {
+  // TODO showAge是false,不应该执行副作用函数
   proxyData.age = 28;
-}, 1000);
+}, 2000);
