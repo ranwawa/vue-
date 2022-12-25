eeac07be6753a186653ff99c454e81a6867a28cb
ranqirong <274544338@qq.com>
2022-12-25 20:12:54 +0800

    7. 抽象后的渲染器

---
+++ index2.js
@@ -0,0 +1,62 @@
+const { effect, ref } = VueReactivity;
+
+function createRenderer(params) {
+  const { createElement, setElementText, insert } = params;
+
+  function mountElement(node, container) {
+    const ele = createElement(node.type);
+
+    if (typeof node.children === "string") {
+      setElementText(ele, node.children);
+    }
+
+    insert(ele, container);
+  }
+
+  function patch(oldNode, newNode, container) {
+    if (oldNode) {
+    } else {
+      mountElement(newNode, container);
+    }
+  }
+
+  function render(node, container) {
+    if (node) {
+      patch(container._node, node, container);
+    } else {
+      if (!container._node) {
+        container.innerHTML = "";
+      }
+    }
+
+    container._node = container.node;
+  }
+
+  return {
+    render,
+  };
+}
+
+const { render } = createRenderer({
+  createElement(type) {
+    return document.createElement(type);
+  },
+  setElementText(el, text) {
+    el.textContent = text;
+  },
+  insert(el, parent, anchor) {
+    parent.insertBefore(el, anchor);
+  },
+});
+
+var count = ref(1);
+
+effect(() => {
+  render(
+    {
+      type: "h1",
+      children: "hello",
+    },
+    document.getElementById("app")
+  );
+});
