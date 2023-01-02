f4e708f82458652e7ff594d3899951398fff267b
ranqirong <274544338@qq.com>
2023-01-02 19:37:23 +0800

    验证设置class三种方式的性能情况

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
 const { effect, ref } = VueReactivity;
 import { render } from "./renderer.js";
 
-render(
-  {
-    type: "div",
-    children: "first",
-  },
-  document.getElementById("app")
-);
+const app = document.querySelector("#app");
+const times = 1000000;
 
-render(
-  {
-    type: "div",
-    children: "second",
-  },
-  document.getElementById("app")
-);
+console.time("setAttribute");
+
+for (let index = 0; index < times; index++) {
+  app.setAttribute("class", "setAttribute");
+}
+
+console.timeEnd("setAttribute");
+
+console.time("className");
+
+for (let index = 0; index < times; index++) {
+  app.className = "className";
+}
+
+console.timeEnd("className");
+
+console.time("classList");
+
+for (let index = 0; index < times; index++) {
+  app.classList = "className";
+}
+
+console.timeEnd("classList");
