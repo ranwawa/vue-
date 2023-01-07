4666db7e19feb4f5e5a730d8c40a1f9057957f0b
ranqirong <274544338@qq.com>
2023-01-07 19:25:53 +0800

    8.12 通过识别children类型,用不同的策略进行更新

--- src/renderer.ts
+++ src/renderer.ts
@@ -8,7 +8,7 @@ type VEI = {
 interface VNode {
   type: string;
   children: string | VNode[];
-  props?: Record<string, unknown>;
+  props: Record<string, unknown>;
   el?: HTMLElement & {
     _node?: VNode;
     _vei?: VEI;
@@ -66,13 +66,117 @@ function createRenderer(params: Params) {
     insert(ele, container);
   }
 
+  function patchChildren(oldNode: VNode, newNode: VNode, container: EL) {
+    enum ChildrenType {
+      "string" = "string",
+      "array" = "array",
+      "null" = "null",
+      "unknown" = "unknown",
+    }
+
+    const getChildrenType = (children: VNode["children"]) => {
+      if (typeof newChildren === "string") {
+        return ChildrenType.string;
+      }
+
+      if (Array.isArray(children)) {
+        return ChildrenType.array;
+      }
+
+      if (children === null) {
+        return ChildrenType.null;
+      }
+
+      return ChildrenType.unknown;
+    };
+
+    const newChildren = newNode.children;
+    const oldChildren = oldNode.children;
+    const newChildrenType = getChildrenType(newChildren);
+    const oldChildrenType = getChildrenType(oldChildren);
+
+    switch (newChildrenType) {
+      case ChildrenType.string:
+        if (oldChildrenType === ChildrenType.array) {
+          (oldChildren as VNode[]).forEach((child) => {
+            unmount(child);
+          });
+        } else {
+          setElementText(container, newChildren as string);
+        }
+        break;
+      case ChildrenType.array:
+        if (oldChildrenType === ChildrenType.array) {
+          (oldChildren as VNode[]).forEach((child) => {
+            unmount(child);
+          });
+        }
+
+        (newChildren as VNode[]).forEach((child) => {
+          mountElement(child, container);
+        });
+        break;
+      case ChildrenType.null:
+        if (oldChildrenType === ChildrenType.array) {
+          (oldChildren as VNode[]).forEach((child) => {
+            unmount(child);
+          });
+        } else {
+          setElementText(container, "");
+        }
+        break;
+      default:
+        console.log("不知道的虚拟节点类型");
+        break;
+    }
+  }
+
+  function patchElement(oldNode: VNode, newNode: VNode) {
+    const ele = (newNode.el = oldNode.el);
+    const oldProps = oldNode.props;
+    const newProps = newNode.props;
+
+    // 更新props
+    for (const key in newProps) {
+      const newValue = newProps[key];
+      const oldValue = oldProps[key];
+
+      newValue !== oldValue && patchProps(ele, key, newValue, shouldSetAsProps);
+    }
+
+    for (const key in oldProps) {
+      const newValue = newProps[key];
+      const oldValue = oldProps[key];
+
+      newValue !== oldValue && patchProps(ele, key, null, shouldSetAsProps);
+    }
+
+    // 更新子节点
+    patchChildren(oldNode, newNode, ele);
+  }
+
   function patch(oldNode: VNode, newNode: VNode, container: HTMLElement) {
-    if (oldNode) {
+    const newType = newNode.type;
+
+    if (oldNode && oldNode.type !== newType) {
       unmount(oldNode);
       oldNode = null;
     }
 
-    mountElement(newNode, container);
+    switch (typeof newType) {
+      // html标签
+      case "string":
+        oldNode
+          ? patchElement(oldNode, newNode)
+          : mountElement(newNode, container);
+        break;
+      // 组件
+      case "object":
+        break;
+      // 其他类型,比如Fragment
+      default:
+        break;
+    }
   }
 
   function unmount(oldVNode: VNode) {
