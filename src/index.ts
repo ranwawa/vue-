c7ed3fa96dd3f86b429c3faf2417b1ecad5cbb29
ranqirong <274544338@qq.com>
2023-01-07 19:25:30 +0800

    8.12 ??仅仅是文本节点改变时,如何提升渲染性能

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,22 @@ const app = document.querySelector<HTMLElement>("#app");
 
 const nums = 10000000;
 
-const div = document.createElement("div");
-div.textContent = "div";
-app.appendChild(div);
+render(
+  {
+    type: "div",
+    children: "div",
+    props: {},
+  },
+  app
+);
 
-console.time("先卸载再挂载");
-for (let index = 0; index < nums; index++) {
-  app.removeChild(div);
-  app.appendChild(div);
-}
-console.timeEnd("先卸载再挂载");
-
-console.time("只赋值文本节点");
-for (let index = 0; index < nums; index++) {
-  app.textContent = "h1-1";
-}
-console.timeEnd("只赋值文本节点");
+render(
+  {
+    type: "div",
+    props: {
+      id: 1,
+    },
+    children: "div2",
+  },
+  app
+);
