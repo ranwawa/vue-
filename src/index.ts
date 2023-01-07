8d04dccaadfdde9370d70ddadca9f39facf18b00
ranqirong <274544338@qq.com>
2023-01-07 16:05:13 +0800

    8.9 同个事件如何绑定多个处理函数

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,15 @@
 const { effect, ref } = VueReactivity;
 import { render } from "./renderer.js";
 
-const app = document.querySelector("#app");
-const times = 1000000;
+const app = document.querySelector<HTMLElement>("#app");
 
-console.time("setAttribute");
-
-for (let index = 0; index < times; index++) {
-  app.setAttribute("class", "setAttribute");
-}
-
-console.timeEnd("setAttribute");
-
-console.time("className");
-
-for (let index = 0; index < times; index++) {
-  app.className = "className";
-}
-
-console.timeEnd("className");
-
-console.time("classList");
-
-for (let index = 0; index < times; index++) {
-  app.classList = "className";
-}
-
-console.timeEnd("classList");
+render(
+  {
+    type: "h1",
+    children: "h1",
+    props: {
+      onClick: [() => console.log(1), () => console.log(2)],
+    },
+  },
+  app
+);
