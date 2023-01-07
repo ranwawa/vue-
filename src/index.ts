ac3449d3e57099946168ea3fb284fc90270d7e4f
ranqirong <274544338@qq.com>
2023-01-07 19:39:18 +0800

    8.13 ??如何添加一个纯粹的文本节点

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,11 @@ import { render } from "./renderer.js";
 
 const app = document.querySelector<HTMLElement>("#app");
 
-const nums = 10000000;
-
 render(
   {
-    type: "div",
-    children: "div",
+    type: "text",
+    children: "文本节点",
     props: {},
-  },
-  app
-);
-
-render(
-  {
-    type: "div",
-    props: {
-      id: 1,
-    },
-    children: "div2",
   },
   app
 );
